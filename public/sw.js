ce901dce36d4f67463692f8fac62ef29a6a9b122
Johan-Potgieter-info <johan.p@live.co.za>
2025-06-16 23:09:32 +0000

    manifest_fixes

--- public/sw.js
+++ public/sw.js
@@ -2,16 +2,20 @@ const CACHE_NAME = 'mia-consent-cache-v1';
 const urlsToCache = [
   '/mia-consent-offline-form-50/',
   '/mia-consent-offline-form-50/index.html',
+  '/mia-consent-offline-form-50/assets/index.css',
+  '/mia-consent-offline-form-50/assets/index.js',
 ];
 
 self.addEventListener('install', event => {
   event.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
   );
 });
 
 self.addEventListener('fetch', event => {
   event.respondWith(
-    caches.match(event.request).then(response => response || fetch(event.request))
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
   );
 });
