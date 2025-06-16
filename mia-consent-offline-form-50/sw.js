7423543e2f50b10fb75f636f132d0a0252f27d58
Johan-Potgieter-info <johan.p@live.co.za>
2025-06-16 22:59:59 +0000

    Updates

---
+++ mia-consent-offline-form-50/sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'mia-consent-cache-v1';
+const urlsToCache = [
+  '/mia-consent-offline-form-50/',
+  '/mia-consent-offline-form-50/index.html',
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
